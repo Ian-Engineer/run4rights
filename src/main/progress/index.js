77790f7f89cb539caadf8251b5930340322b8530
Ian Swensson <ianswensson@Ians-MacBook-Pro.local>
2025-02-08 08:20:21 -0700

    total temp

--- src/main/progress/index.js
+++ src/main/progress/index.js
@@ -8,22 +8,31 @@ import { Box, Button, Typography, Skeleton } from "@mui/material";
 import theme from "../../config/style/theme";
 import config from "../../config";
 
+const cant_figure_ws_out = true
+
 const ProgressPage = () => {
   const [loading, setLoading] = useState(true);
   const [total, setTotal] = useState(0);
 
   useEffect(()=>{
-    const socket = new WebSocket(config.development ? "ws://localhost:4000" : "wss://api.run4rights.com"); // Change to your actual server URL
+    
+    if (cant_figure_ws_out) {
+      setTotal(1685);
+      setLoading(false);
+    } else {
+      const socket = new WebSocket(config.development ? "ws://localhost:4000" : "wss://api.run4rights.com"); // Change to your actual server URL
+  
+      socket.onmessage = (event) => {
+          const total = JSON.parse(event.data).data;
+          console.log(total)
+          setTotal(total)
+          setLoading(false)
+      };
+  
+      socket.onopen = () => {
+        console.log('connected')
+      }
 
-    socket.onmessage = (event) => {
-        const total = JSON.parse(event.data).data;
-        console.log(total)
-        setTotal(total)
-        setLoading(false)
-    };
-
-    socket.onopen = () => {
-      console.log('connected')
     }
   },[])
 
